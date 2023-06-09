37b5ed50c3692fa9732d2b46f76c3c3c2fcc65e6
annadavydova <lileynik@yahoo.com>
2023-06-09 13:21:35 -0400

    finished 2 module:1,6

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,21 +1,25 @@
-import React from "react";
+import React, {Suspense} from "react";
 import "./styles/index.scss";
 import {AppRouter} from "app/providers/router";
 import {Navbar} from "widgets/Navbar";
 import {useTheme} from "app/providers/ThemeProvider";
 import {classNames} from "shared/lib/classNames/classNames";
-
-
-
+import {Sidebar} from "widgets/Sidebar";
 
 const App = () => {
-const {theme}=useTheme();
+    const {theme} = useTheme();
     return (
-        <div className={classNames('app', {},[theme])}>
+        <div className={classNames('app', {}, [theme])}>
+<Suspense fallback="">
+    <Navbar/>
+    <div className='content-page'>
+        <Sidebar/>
+        <AppRouter/>
+    </div>
+</Suspense>
 
 
-     <Navbar/>
-      <AppRouter/>
+
 
         </div>
     );
