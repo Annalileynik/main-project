c4045b6149dc6f21697345db502a684dd9825aa4
annadavydova <lileynik@yahoo.com>
2023-06-09 15:08:30 -0400

    finished 2 add:babel

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -9,20 +9,26 @@ export function buildLoaders({isDev}: BuildOptions): webpack.RuleSetRule[] {
         use: ['@svgr/webpack'],
     }
 
-    const fileLoader =   {
-            test: /\.(png|jpe?g|gif|woff2|woff)$/i,
-            use: [
-                {
-                    loader: 'file-loader',
-                },
-            ],
-        }
-
-    const typescriptLoaders = {
-        test: /\.tsx?$/,
-        use: 'ts-loader',
+    const babelLoader = {
+        test: /\.(js|jsx|tsx)$/,
         exclude: /node_modules/,
+        use: {
+            loader: "babel-loader",
+            options: {
+                presets: ['@babel/preset-env'],
+                "plugins": [
+                    [
+                        "i18next-extract",
+                        {
+                            locales: ['ru', 'en'],
+                            keyAsDefaultValue: true
+                        }
+                    ],
+                ]
+            }
+        }
     }
+
     const cssLoader = {
         test: /\.s[ac]ss$/i,
         use: [
@@ -33,19 +39,36 @@ export function buildLoaders({isDev}: BuildOptions): webpack.RuleSetRule[] {
                     modules: {
                         auto: (resPath: string) => Boolean(resPath.includes('.module.')),
                         localIdentName: isDev
-                            ? "[path][name]__[local]--[hash:base64:5]"
+                            ? '[path][name]__[local]--[hash:base64:5]'
                             : '[hash:base64:8]'
-                    }
+                    },
                 }
             },
             "sass-loader",
         ],
     }
-    return [
-        typescriptLoaders,
-        cssLoader,
-        svgLoader,
-        fileLoader
 
+    // Если не используем тайпскрипт - нужен babel-loader
+    const typescriptLoader = {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+    }
+
+    const fileLoader = {
+        test: /\.(png|jpe?g|gif|woff2|woff)$/i,
+        use: [
+            {
+                loader: 'file-loader',
+            },
+        ],
+    }
+
+    return [
+        fileLoader,
+        svgLoader,
+        babelLoader,
+        typescriptLoader,
+        cssLoader,
     ]
 }
