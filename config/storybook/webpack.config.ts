bb76d5c5e8221e868f1e62ef9b10220f40a1ae58
annadavydova <lileynik@yahoo.com>
2023-06-18 19:09:19 -0400

    change config

--- config/storybook/webpack.config.ts
+++ config/storybook/webpack.config.ts
@@ -1,5 +1,6 @@
 import webpack, { RuleSetRule } from 'webpack';
 import path from 'path';
+// @ts-ignore
 import { buildCssLoader } from '../build/loaders/buildCssLoader';
 import { BuildPaths } from '../build/types/config';
 
