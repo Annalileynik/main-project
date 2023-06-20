e8ffa1342cf095eda11d4170fddf10823136a67c
annadavydova <lileynik@yahoo.com>
2023-06-20 15:52:24 -0400

    add change Navbar

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,6 +1,5 @@
 import { classNames } from 'shared/lib/classNames/classNames';
 import React from 'react';
-import { AppLink, AppLinkTheme } from 'shared/ui/AppLink/AppLink';
 import cls from './Navbar.module.scss';
 
 interface NavbarProps {
@@ -10,7 +9,7 @@ export function Navbar({ className }:NavbarProps) {
     return (
         <div className={classNames(cls.Navbar, {}, [className])}>
             <div className={cls.links}>
-      /
+                /
             </div>
         </div>
 
